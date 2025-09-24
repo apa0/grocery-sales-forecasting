7ffae396baee881045ef6ff0c8fbaa7d4347b753
Angie Diaz <adiazhe@clemson.edu>
2025-09-24 12:48:18 -0400

    added description for visualization

---
+++ scripts/visualize.py
@@ -0,0 +1,12 @@
+# This script is to visualize our data for EDA, ideas listed below
+
+r"""
+Outcome (unit_sales):
+	•	Line chart: average daily sales over time.
+	•	Histogram: distribution of daily sales (likely skewed).
+Predictor 1 (onpromotion):
+	•	Boxplot or bar plot: avg sales on promo vs. not on promo.
+Predictor 2 (holiday or perishable):
+	•	Compare avg sales on holidays vs. normal days.
+	•	Or compare perishables vs. non-perishables.
+"""
